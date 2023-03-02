51eb381767f8cad783c13ff4645d2161564f64c4
Nicolas-Gut <nicolasgutprincipal@gmail.com>
2023-03-02 17:38:38 -0300

    commit 2

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,8 @@
 import Box from '@mui/material/Box'
+import { width } from '@mui/system'
+import FotoMontanha from './assets/img/FotoMontanha.jpeg'
+import { ReactComponent as IconCarrinho} from './assets/img/carrinho.svg'
+import { BsCartPlusFill as IconCarrinhoMais } from 'react-icons/bs'
 
 const Sidebar = () => {
     return(
@@ -8,6 +12,37 @@ const Sidebar = () => {
             height: 500
         }}>
             <h2>Sidebar</h2>
+            <img
+            style={{
+                widht: 180,
+                height: 123.83
+            }}
+            src="https://i.natgeofe.com/k/830b5d15-92db-429f-a80a-cc89b5700af5/mt-everest.jpg?w=374&h=257" alt="Mountain"
+            />
+            <hr/>
+            <img
+            style={{
+                width: 180,
+                heigh: 120
+            }}
+            src={FotoMontanha}
+            alt="Mountain"
+            />
+            <hr/>
+            <IconCarrinho
+            style={{
+                width: 30,
+                height: 30
+            }}
+            />
+            <hr/>
+            <IconCarrinhoMais 
+            style={{
+                width: 40,
+                height: 40,
+                color: 'red'
+            }}
+            />
         </Box>
     )
 }
